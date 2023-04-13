441270c3c6779ee2e61a666e75dfff8533c4e6e8
Cameron <Cameron@dansclan.com>
2023-04-13 11:38:11 -0700

    Chapter 3-1 - 3-3

---
+++ chapter-3/greetings.py
@@ -0,0 +1,9 @@
+names = ["cameron", "evie", "nate", "seth"]
+message = f"Greetings, {names[0].title()}."
+print(message)
+message = f"Greetings, {names[1].title()}."
+print(message)
+message = f"Greetings, {names[2].title()}."
+print(message)
+message = f"Greetings, {names[3].title()}."
+print(message)
---
+++ chapter-3/your-own-list.py
@@ -0,0 +1,3 @@
+cars = ["Subaru", "Mitsubishi", "Honda"]
+message = f"I would like to own a {cars[0]} or a {cars[1]}.\nI currently own a {cars[2]}."
+print(message)
