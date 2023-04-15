d19a5bae45b5a17491f67daf1609a0fca3e5be81
Cameron <Cameron@dansclan.com>
2023-04-14 22:25:06 -0700

    Chapter 3-8 - 3-11

---
+++ chapter-3/dinner-guests.py
@@ -0,0 +1,26 @@
+guests = ["evie", "seth", "nate"]
+message = f"Hey {guests[0].title()}, Would you like to go to dinner?"
+print(message)
+message = f"Hey {guests[1].title()}, Would you like to go to dinner?"
+print(message)
+message = f"Hey {guests[2].title()}, Would you like to go to dinner?"
+print(message)
+message = f"Hey everyone I found a bigger table."
+print(message)
+guests.insert(0, "jordan")
+guests.insert(2, "alfred")
+guests.append("bryan")
+message = f"Hey {guests[0].title()}, Would you like to go to dinner?"
+print(message)
+message = f"Hey {guests[1].title()}, Would you like to go to dinner?"
+print(message)
+message = f"Hey {guests[2].title()}, Would you like to go to dinner?"
+print(message)
+message = f"Hey {guests[3].title()}, Would you like to go to dinner?"
+print(message)
+message = f"Hey {guests[4].title()}, Would you like to go to dinner?"
+print(message)
+message = f"Hey {guests[5].title()}, Would you like to go to dinner?"
+print(message)
+message = f"There are {len(guests)} guests going to the dinner."
+print(message)
