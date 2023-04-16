8bd54ea659b2916e2d137bc87836ff5d863ceb99
Cameron <Cameron@dansclan.com>
2023-04-15 23:07:02 -0700

    Chapter 4-1 - 4-2

---
+++ chapter-4/animals.py
@@ -0,0 +1,8 @@
+animals = ["dog", "cat", "fish"]
+
+for animal in animals:
+    message = f"A {animal.title()} would make a great pet!"
+    print(message)
+
+message = "Any of these animals would make a great pet!"
+print(message)
---
+++ chapter-4/pizzas.py
@@ -0,0 +1,8 @@
+pizzas = ["pepperoni", "cheese", "hawaiian"]
+
+for pizza in pizzas:
+    message = f"I love {pizza.title()} pizza!"
+    print(message)
+
+message = "I love pizza!"
+print(message)
